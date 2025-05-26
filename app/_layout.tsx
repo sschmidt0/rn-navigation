4f09c5fd6144978f81c34e1f65c9e8912cdabff2
sschmidt0 <sschmidt0@uoc.edu>
2025-05-26 20:07:32 +0200

    initial state for presentation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,36 +1,36 @@
 import React from "react";
 import { Slot, Stack } from "expo-router";
 
-// const RootLayout = () => {
-//   return <Slot />;
-// };
-
 const RootLayout = () => {
-  return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#f4511e",
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "bold",
-        },
-      }}
-    >
-      <Stack.Screen name="index" options={{ headerShown: true }} />
-      <Stack.Screen name="settings" options={{ title: "Ajustes" }} />
-      <Stack.Screen
-        name="profile"
-        options={{ title: "Perfil", headerBackTitle: "Inicio" }}
-      />
-      <Stack.Screen
-        name="products/index"
-        options={{ title: "Productos", headerBackTitle: "Inicio" }}
-      />
-      <Stack.Screen name="products/[id]" />
-    </Stack>
-  );
+  return <Slot />;
 };
+
+// const RootLayout = () => {
+//   return (
+//     <Stack
+//       screenOptions={{
+//         headerStyle: {
+//           backgroundColor: "#f4511e",
+//         },
+//         headerTintColor: "#fff",
+//         headerTitleStyle: {
+//           fontWeight: "bold",
+//         },
+//       }}
+//     >
+//       <Stack.Screen name="index" options={{ headerShown: true }} />
+//       <Stack.Screen name="settings" options={{ title: "Ajustes" }} />
+//       <Stack.Screen
+//         name="profile"
+//         options={{ title: "Perfil", headerBackTitle: "Inicio" }}
+//       />
+//       <Stack.Screen
+//         name="products/index"
+//         options={{ title: "Productos", headerBackTitle: "Inicio" }}
+//       />
+//       <Stack.Screen name="products/[id]" />
+//     </Stack>
+//   );
+// };
 
 export default RootLayout;
